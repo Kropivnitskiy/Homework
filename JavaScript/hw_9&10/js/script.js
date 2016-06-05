11039c08f6302e5d844053fed331c0dc31ca9b76
Ivan Kropivnitskiy <ivam.kropivnitskiy@gmail.com>
2016-06-05 21:55:10 +0300

    Fixed HW 9&10

--- JavaScript/hw_9&10/js/script.js
+++ JavaScript/hw_9&10/js/script.js
@@ -1,17 +1,19 @@
-// $(function() {
-//     $('.jcarousel').jcarousel({
-//         // Core configuration goes here
-//     });
+// select cusel
+jQuery(document).ready(function(){
+var params = {
+        changedEl: ".lineForm select",
+        visRows: 4,
+        scrollArrows: true
+    }
+    cuSel(params);
+    
+    var params = {
+        changedEl: "#films",
+        scrollArrows: false
+    }
+    cuSel(params);
 
-//     $('.prev prev-stage').jcarouselControl({
-//         target: '-=1'
-//     });
-
-//     $('.next next-stage').jcarouselControl({
-//         target: '+=1'
-//     });
-// });
-
+// fancybox
 $(function() {
   $(".fancybox").fancybox();
 });
